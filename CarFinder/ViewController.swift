fa7cd1a24b65ebf5c91911b4487a52e76fd49a84
Nathan Lorenz <nathanlorenz0417@gmail.com>
2018-01-02 13:22:58 -0800

    Initial Commit

---
+++ CarFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarFinder
+//
+//  Created by Nathan Lorenz on 2018-01-02.
+//  Copyright © 2018 Nathan Lorenz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
